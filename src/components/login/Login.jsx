0b62b8712b3c2b3438a47c75eae4ecc6929881f3
Dewa <uchiasasuke123.sp@gmail.com>
2023-01-01 22:10:19 +0700

    Update Login Page Style

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,12 +2,19 @@ import './login.css';
 
 function Login() {
     return (
-      <div className="">
-        <div className="login-box-left">
+      <div className="container-fluid d-flex row">
+        <div className="login-box-left col-md-5 d-flex justify-content-center align-items-center">
           <img src="images/unindra-logo.png" alt="Logo Unindra"></img>
         </div>
-        <div className="flex">
-          <input type="text" className="w-100" />
+        <div className="col-md-7 d-flex justify-content-center align-items-center">
+          <form className="w-50">
+          <div class="mb-3 row">
+            <input type="text" className="form-control px-2 w-100" placeholder="Username"/>
+          </div>
+          <div class="mb-3 row">
+            <input type="text" className="form-control px-2 w-100" placeholder="Password"/>
+          </div>
+          </form>
         </div>
       </div>
     );
