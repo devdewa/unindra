e1a4b26a64ce025731aa51c30649db21904888f1
Dewa <uchiasasuke123.sp@gmail.com>
2023-01-02 10:13:30 +0700

    Add title in Login Page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -6,14 +6,15 @@ function Login() {
         <div className="login-box-left col-md-5 d-flex justify-content-center align-items-center">
           <img src="images/unindra-logo.png" alt="Logo Unindra"></img>
         </div>
-        <div className="col-md-7 d-flex justify-content-center align-items-center">
+        <div className="col-md-7 d-flex justify-content-center align-items-center flex-wrap">
           <form className="w-50">
-          <div class="mb-3 row">
-            <input type="text" className="form-control px-2 w-100" placeholder="Username"/>
-          </div>
-          <div class="mb-3 row">
-            <input type="text" className="form-control px-2 w-100" placeholder="Password"/>
-          </div>
+            <h2 className="mx-auto mb-5 d-flex justify-content-center">Welcome</h2>
+            <div class="mb-3 row">
+              <input type="text" className="form-control px-2 w-100 mb-3" placeholder="Username"/>
+            </div>
+            <div class="mb-3 row">
+              <input type="text" className="form-control px-2 w-100" placeholder="Password"/>
+            </div>
           </form>
         </div>
       </div>
